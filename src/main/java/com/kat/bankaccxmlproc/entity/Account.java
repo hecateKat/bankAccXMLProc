59ff13b54916ee2a81ca804e67e4921872e24403
Kat <kat.ochmanska@gmail.com>
2020-02-12 15:07:30 +0100

    added annotations

--- src/main/java/com/kat/bankaccxmlproc/entity/Account.java
+++ src/main/java/com/kat/bankaccxmlproc/entity/Account.java
@@ -1,5 +1,10 @@
 package com.kat.bankaccxmlproc.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,10 +12,21 @@ import java.time.LocalDate;
 @Data
 public class Account {
 
+    @JacksonXmlProperty(isAttribute = true)
     private String iban;
+
+    @JacksonXmlProperty
     private String name;
+
+    @JacksonXmlProperty
     private String currency;
+
+    @JacksonXmlProperty
     private Double balance;
+
+    @JacksonXmlProperty
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate closingDate;
 
     public Account() {
--- src/main/java/com/kat/bankaccxmlproc/model/AccountContainer.java
+++ src/main/java/com/kat/bankaccxmlproc/model/AccountContainer.java
@@ -1,11 +1,19 @@
 package com.kat.bankaccxmlproc.model;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.kat.bankaccxmlproc.entity.Account;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
+@JacksonXmlRootElement(localName = "accounts")
 public class AccountContainer {
 
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "account")
     private List<Account> accounts;
 
     public AccountContainer(List<Account> accounts) {
