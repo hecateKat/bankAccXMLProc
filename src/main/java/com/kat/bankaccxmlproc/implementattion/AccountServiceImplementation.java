32958a828d03b6be5dc06e9a68743a9308440240
Kat <kat.ochmanska@gmail.com>
2020-02-12 12:34:40 +0100

    added implementation to AccountService

---
+++ src/main/java/com/kat/bankaccxmlproc/implementattion/AccountServiceImplementation.java
@@ -0,0 +1,33 @@
+package com.kat.bankaccxmlproc.implementattion;
+
+import com.kat.bankaccxmlproc.entity.Account;
+import com.kat.bankaccxmlproc.service.AccountService;
+
+import java.util.List;
+
+public class AccountServiceImplementation implements AccountService {
+    @Override
+    public AccountService checkPlnCurrency() {
+        return null;
+    }
+
+    @Override
+    public AccountService checkIfCurrencyBelowZero() {
+        return null;
+    }
+
+    @Override
+    public AccountService filterExpitedAcc() {
+        return null;
+    }
+
+    @Override
+    public AccountService checkIbanNumber() {
+        return null;
+    }
+
+    @Override
+    public List<Account> addAccountsInOrder() {
+        return null;
+    }
+}
